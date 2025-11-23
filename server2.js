8bf5b7f8f10838e4645e81edb973b0a527e36a34
Vivek <vivekdubey4305@gmail.com>
2025-11-23 13:15:29 +0530

    shift mongodb to online server

--- server2.js
+++ server2.js
@@ -4,9 +4,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 // const Person = require('./models/Person');
 // const Menuitem = require('./models/Menuitem');
